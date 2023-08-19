471406019774cf5d4d1ccff56a4ace0c05073554
Nelson Morales <camilomorales2615@gmail.com>
2023-08-19 14:57:41 -0500

    rebuild

--- src/App.jsx
+++ src/App.jsx
@@ -26,8 +26,8 @@ function App() {
         {
           overview ? <Overview /> : (content ? <Content /> : <Create />)
         }
-      </DataProvider>
       <Footer/>
+      </DataProvider>
     </>
   )
 }
