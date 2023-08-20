28f19e2499de0669ee2fbf614249c2ccca661372
Nelson Morales <camilomorales2615@gmail.com>
2023-08-20 18:03:33 -0500

    Solución Error ortografico

--- src/context/DataContext.jsx
+++ src/context/DataContext.jsx
@@ -4,7 +4,7 @@ export const DataContext = createContext();
 
 const data1 = [
     {
-        sport: "Futbol",
+        sport: "Football",
         imagen: "https://upload.wikimedia.org/wikipedia/commons/b/b4/Lionel-Messi-Argentina-2022-FIFA-World-Cup_%28cropped%29.jpg",
         nombre: "Lionel Messi",
         numero: "10",
